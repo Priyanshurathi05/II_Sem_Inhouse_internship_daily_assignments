632495fcc21e68c8d4e8e4cfc3e90d73ff77506a
Priyanshurathi05 <priyanshurathii336@gmail.com>
2026-07-12 13:39:23 +0530

    Add files via upload

---
+++ DAY7/registration.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_GET["name"];
+$branch =$_GET["branch"];
+$email = $_GET["email"];
+$phonenumber =$_GET["phoneNumber"];
+
+
+echo "values recived:$name $branch $email $phonenumber";
+
+?>
---
+++ DAY7/welcome.php
@@ -0,0 +1,26 @@
+<html>
+    <body>
+        <?php
+
+
+$name ="Niyati Sharma";
+$cgpa = 9.0;
+$branch= "CSE (DS)";
+$year= date("Y");
+$month = date("m");
+$prev_year=$year-1;
+$next_year=$year+1;
+
+if($month <7){
+    echo "Year $year-$next_year";
+}else{
+    echo "Year $prev_year-$year";
+}
+?>
+<h1><?= $name?> </h1>
+<p><?=$cgpa?></p>
+<p><?=$branch?></p>
+<p>Date:<?=date("y-m-d H:i:s")?></p>
+<p>you are visiting from: <?=$_SERVER["REMOTE_ADDR"]?><p>
+</body>
+</html>
